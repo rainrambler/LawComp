934ce2761cce73fcbf32d638e78dccffb0411737
rainrambler <wanganyu@outlook.com>
2022-09-29 19:38:32 +0800

    Update law_compare.py

--- law_compare.py
+++ law_compare.py
@@ -72,7 +72,7 @@ class CnLaw:
     def parse_section(self, content):
         ''' eg: 第一节　一般规定
         '''
-        print('==> Parsing section ', content[:30], '...')
+        #print('==> Parsing section ', content[:30], '...')
         if re.search(r'第[〇一二三四五六七八九]+节', content) is None:
             self.cur_section = ""
             self.parse_article(content)
@@ -114,7 +114,7 @@ class CnLaw:
     def parse_article(self, content):
         ''' eg: 第十三条　符合下列...
         '''
-        print('==> Parsing article ', content[:30], '...')
+        #print('==> Parsing article ', content[:30], '...')
         arr = re.finditer(r'第[〇一二三四五六七八九十]+条', content)
         indices = []
         parts = []
@@ -164,8 +164,239 @@ class CnLaw:
     def parse_article_clause(self, content):
         ''' eg: （三）为履行法定职责或者法定义务所必需；
         '''
-        print('==> Parsing clause ', content[:30], '...')
+        #print('==> Parsing clause ', content[:30], '...')
         arr = re.finditer(r'（[〇一二三四五六七八九十]+）', content)
+        indices = []
+        parts = []
+        clause_ids = []
+
+        for item in arr:
+            indices.append(item.span()[0])
+            clause_ids.append(item.group(0))
+
+        mainclause = content[:indices[0]]
+        i = 0
+        while i < len(indices)-1:
+            part = content[indices[i]:indices[i+1]]
+            parts.append(mainclause + part)
+            i+=1
+        last = content[indices[len(indices)-1]:]
+        parts.append(mainclause+last)
+
+        totalcount = len(clause_ids)
+        if totalcount != len(parts):
+            print(f"WARN: Clauses Len: {totalcount} and {len(parts)}.")
+            return
+        for i in range(totalcount):
+            one_clause = parts[i]
+            cur_id = self.create_id()
+            self.id2content[cur_id] = one_clause
+
+class EuLaw:
+    '''EU GDPR
+    EN: CHAPTER III / Section 1 / Article 12 / 1. / (a) 
+    ZH: 第三章 / 第一部分 / 第12条 / 1． / (a)
+    '''
+    cur_chapter = ""
+    cur_article = ""
+    cur_subarticle = ""
+    id2content = {}
+
+    def print_contents(self):
+        ''' Print contents '''
+        for k, v in self.id2content.items():
+            print(f'[{k}]:{v[0:30]}')
+    
+    def parse_chapter(self, content):
+        '''Parse each chapter. eg: 第二章 原则'''
+        arr = re.finditer(r'第[〇一二三四五六七八九]+章', content)
+        indices = []
+        chapters = []
+        chapter_ids = []
+
+        for chapt in arr:
+            indices.append(chapt.span()[0])
+            chapter_ids.append(chapt.group(0))
+        #print(chapter_ids)
+        i = 0
+        while i < len(indices)-1:
+            part = content[indices[i]:indices[i+1]]
+            chapters.append(part)
+            i+=1
+        last = content[indices[len(indices)-1]:]
+        chapters.append(last)
+
+        totalcount = len(chapter_ids)
+        if totalcount != len(chapters):
+            print(f"WARN: Chapter Len: {totalcount} and {len(chapters)}.")
+            return
+
+        for i in range(totalcount):
+            one_chapter = chapters[i]
+            self.cur_chapter = chapter_ids[i]
+            sections = remove_first_line(one_chapter)
+            self.parse_section(sections)
+
+    def parse_section(self, content: str):
+        ''' eg: 第一部分 透明性与模式
+        '''
+        #print('==> Parsing section ', content[:30], '...')
+        arr = content.splitlines()
+        section_content = ""
+        if len(arr) <= 1:
+            section_content = content
+            self.parse_article(section_content)
+            return
+
+        for a_sec in arr:
+            if re.search(r'第[〇一二三四五六七八九]+部分', a_sec) is None:
+                section_content = section_content + a_sec + '\r\n'
+
+        self.parse_article(section_content)
+
+    def parse_article(self, content):
+        ''' eg: 第12条 信息...
+        '''
+        #print('==> Parsing article ', content[:30], '...')
+        arr = re.finditer(r'第[0-9]+条', content)
+        indices = []
+        parts = []
+        article_ids = []
+
+        for item in arr:
+            indices.append(item.span()[0]) # pos in string
+            article_ids.append(item.group(0)) # title, eg. 第12条 
+
+        if len(indices) == 0:
+            return
+
+        i = 0
+        while i < len(indices)-1:
+            part = content[indices[i]:indices[i+1]]
+            parts.append(part)
+            i+=1
+        last = content[indices[len(indices)-1]:]
+        parts.append(last)
+
+        totalcount = len(article_ids)
+        if totalcount != len(parts):
+            print(f"WARN: Articles Len: {totalcount} and {len(parts)}.")
+            return
+
+        for i in range(totalcount):
+            one_article = parts[i]
+            self.cur_article = article_ids[i]
+
+            if self.has_sub_article(one_article):
+                self.parse_sub_article(one_article)
+            else:
+                cur_id = self.create_id()
+                self.add_article(cur_id, one_article)
+
+    def add_article(self, a_id: int, article: str):
+        '''Add an article'''
+        self.id2content[a_id] = article
+
+    def parse_sub_article(self, content):
+        ''' eg: 第7条 同意的条件 ==> 1．当处理
+        '''
+        #print('==> Parsing sub article ', content[:30], '...')
+        arr = re.finditer(r'[0-9]+.', content)
+        indices = []
+        parts = []
+        article_ids = []
+
+        for item in arr:
+            indices.append(item.span()[0]) # pos in string
+            article_ids.append(item.group(0)) # title, eg. 1．当处理
+
+        print(article_ids)
+        print('=-=-=-=')
+        if len(indices) == 0:
+            return
+
+        i = 0
+        while i < len(indices)-1:
+            part = content[indices[i]:indices[i+1]]
+            parts.append(part)
+            i+=1
+        last = content[indices[len(indices)-1]:]
+        parts.append(last)
+
+        totalcount = len(article_ids)
+        if totalcount != len(parts):
+            print(f"WARN: Sub Articles Len: {totalcount} and {len(parts)}.")
+            return
+
+        for i in range(totalcount):
+            one_article = parts[i]
+            self.cur_subarticle = article_ids[i]
+
+            if self.has_point(one_article):
+                #self.parse_point(one_article)
+                # TODO
+                cur_id = self.create_id()
+                self.id2content[cur_id] = one_article
+            else:
+                cur_id = self.create_id()
+                self.id2content[cur_id] = one_article
+
+    def parse_point(self, content):
+        ''' eg: (a)第五章规定的
+        '''
+        #print('==> Parsing point ', content[:30], '...')
+        arr = re.finditer(r'([a-z]+)', content)
+        indices = []
+        parts = []
+        point_ids = []
+
+        for item in arr:
+            indices.append(item.span()[0]) # pos in string
+            point_ids.append(item.group(0)) # title, eg. (a)第五章规定的
+
+        print(point_ids)
+        print('=======')
+        if len(indices) == 0:
+            return
+
+        i = 0
+        while i < len(indices)-1:
+            part = content[indices[i]:indices[i+1]]
+            parts.append(part)
+            i+=1
+        last = content[indices[len(indices)-1]:]
+        parts.append(last)
+
+        totalcount = len(point_ids)
+        if totalcount != len(parts):
+            print(f"WARN: Points Len: {totalcount} and {len(parts)}.")
+            return
+
+        for i in range(totalcount):
+            one_article = parts[i]
+            cur_id = self.create_id()
+            self.id2content[cur_id] = one_article
+
+    def create_id(self):
+        ''' Make id in dict'''
+        if len(self.cur_subarticle) == 0:
+            return self.cur_chapter + "_" + self.cur_article
+        else:
+            return self.cur_chapter + "_" + self.cur_article + " (" + self.cur_subarticle + ')'
+
+    def has_point(self, content):
+        ''' Does article have clause '''
+        return re.search(r'([a-z]+)', content) is not None
+
+    def has_sub_article(self, content):
+        ''' Does article have clause '''
+        return re.search(r'[0-9]+.', content) is not None
+
+    def parse_article_clause(self, content):
+        ''' eg: 1．对于和...
+        '''
+        #print('==> Parsing clause ', content[:30], '...')
+        arr = re.finditer(r'[0-9]+.', content)
         indices = []
         parts = []
         clause_ids = []
@@ -210,13 +441,20 @@ def remove_first_line(content):
         return content[idx+1:]
 
 def compare_gdpr_vs_cn():
+    ''' compare EU and cn privacy law '''
     content = read_text_file('CN PIPL Stanford ZH.txt')
     alaw = CnLaw()
     alaw.parse_chapter(content)
     alaw.print_clauses()
+
+    content = read_text_file('EU GDPR.txt')
+    eu_law = EuLaw()
+    eu_law.parse_chapter(content)
+    eu_law.print_contents()
     #print(content)
 
 def main():
+    ''' Entrance '''
     compare_gdpr_vs_cn()
 
 if __name__ == "__main__":
