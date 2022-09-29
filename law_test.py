0d36f30ce60c1eae93d83a8f555cc2e816ba9ffc
rainrambler <wanganyu@outlook.com>
2022-09-29 11:11:33 +0800

    Update law_test.py

--- law_test.py
+++ law_test.py
@@ -2,10 +2,19 @@ import unittest
 import law_compare
 
 class StringTest(unittest.TestCase):
-    # Returns True or False. 
-    def testConvert(self):
+    def test_convert(self):
         v = law_compare.chstring2int('五十一')
         self.assertTrue(v == 51)
+
+    def test_remove_first_line(self):
+        content = 'aa\r\nbb\r\ncc'
+        v = law_compare.remove_first_line(content)
+        self.assertTrue(v == 'bb\r\ncc')
+
+    def test_remove_first_line2(self):
+        content = 'aabbcc'
+        v = law_compare.remove_first_line(content)
+        self.assertTrue(v == content)
 
 if __name__ == "__main__":
     unittest.main()
