3f2c5cb0d39357cc9e0e580e4e028579e0f06e8e
rainrambler <wanganyu@outlook.com>
2022-09-30 18:20:12 +0800

    refactor

--- law_test.py
+++ law_test.py
@@ -1,46 +1,46 @@
 import unittest
-import law_compare
 import re
+import content_handler
 
 class StringTest(unittest.TestCase):
     ''' testcase of string functions '''
     def test_convert(self):
-        v = law_compare.chstring2int('五十一')
+        v = content_handler.chstring2int('五十一')
         self.assertTrue(v == 51)
 
     def test_remove_first_line(self):
         content = 'aa\r\nbb\r\ncc'
-        v = law_compare.remove_first_line(content)
+        v = content_handler.remove_first_line(content)
         self.assertTrue(v == 'bb\r\ncc')
 
     def test_remove_first_line2(self):
         content = 'aabbcc'
-        v = law_compare.remove_first_line(content)
+        v = content_handler.remove_first_line(content)
         self.assertTrue(v == content)
 
     def test_re_demo(self):
         content = '1. aaa'
-        v = law_compare.re_demo(content)
+        v = content_handler.re_demo(content)
         self.assertTrue(v == '1.')
 
     def test_re_demo2(self):
         content = 'ha'
-        v = law_compare.re_demo(content)
+        v = content_handler.re_demo(content)
         self.assertTrue(v == '')
 
     def test_re_demo3(self):
         content = 'aa 22. bb'
-        v = law_compare.re_demo(content)
+        v = content_handler.re_demo(content)
         self.assertTrue(v == '22.')
 
     def test_has_sub_article_1(self):
         content = 'aa 22． bb'
-        v = law_compare.has_sub_article(content)
+        v = content_handler.has_sub_article(content)
         self.assertTrue(v is True)
 
     def test_has_sub_article_2(self):
         content = 'aa . bb'
-        v = law_compare.has_sub_article(content)
+        v = content_handler.has_sub_article(content)
         self.assertTrue(v is False)
 
     def test_has_sub_article_3(self):
@@ -52,36 +52,36 @@ class StringTest(unittest.TestCase):
 2．领导性监管机构可以随时要求其他相关监管机构提供第61条规定的互助合作，而且可以根据第62条而进行联合行动，这尤其适用于如下情形：为了进行调查，或者为了实施涉及到设立在另一成员国的控制者或处理者的措施。
 
         '''
-        v = law_compare.has_sub_article(content)
+        v = content_handler.has_sub_article(content)
         self.assertTrue(v is True)
 
     def test_is_valid_article_index_1(self):
         content = '第1条 aa'
         mo = re.search(r'第[0-9]+条', content)
         start_pos = mo.span()[0] # pos in string
-        v = law_compare.is_valid_article_index(content, start_pos)
+        v = content_handler.is_valid_title_position(content, start_pos)
         self.assertTrue(v)
 
     def test_is_valid_article_index_2(self):
         content = '参考第10条'
         mo = re.search(r'第[0-9]+条', content)
         start_pos = mo.span()[0] # pos in string
-        v = law_compare.is_valid_article_index(content, start_pos)
+        v = content_handler.is_valid_title_position(content, start_pos)
         self.assertFalse(v)
 
     def test_has_point_1(self):
         content = '(a) xx'
-        v = law_compare.has_point(content)
+        v = content_handler.has_point(content)
         self.assertTrue(v)
 
     def test_has_point_2(self):
         content = 'axx'
-        v = law_compare.has_point(content)
+        v = content_handler.has_point(content)
         self.assertFalse(v)
 
     def test_has_point_3(self):
         content = '123 (a) xx'
-        v = law_compare.has_point(content)
+        v = content_handler.has_point(content)
         self.assertTrue(v)
 
 if __name__ == "__main__":
